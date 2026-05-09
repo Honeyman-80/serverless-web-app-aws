0b76ff935da6487311a522bbfd24068d788bbefa
Honeyman-80 <david.w.honeyman@gmail.com>
2026-05-09 16:55:22 -0600

    Update app.js

--- frontend/app.js
+++ frontend/app.js
@@ -1,6 +1,13 @@
 const button = document.getElementById("testButton");
 const message = document.getElementById("message");
 
-button.addEventListener("click", () => {
-  message.textContent = "Button clicked!";
+button.addEventListener("click", async () => {
+
+  const response = await fetch(
+    "https://jkr4rh399c.execute-api.us-east-1.amazonaws.com/hello"
+  );
+
+  const data = await response.json();
+
+  message.textContent = data.message;
 });
