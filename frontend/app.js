ccbe34ce8b57fffb49598fd49f11680abe8900d9
Honeyman-80 <david.w.honeyman@gmail.com>
2026-05-09 18:54:13 -0600

    Update app.js

--- frontend/app.js
+++ frontend/app.js
@@ -1,10 +1,24 @@
 const button = document.getElementById("testButton");
 const message = document.getElementById("message");
+const messageInput = document.getElementById("messageInput");
 
 button.addEventListener("click", async () => {
 
+  const userMessage = messageInput.value;
+
   const response = await fetch(
-    "https://jkr4rh399c.execute-api.us-east-1.amazonaws.com/hello"
+    "https://jkr4rh399c.execute-api.us-east-1.amazonaws.com/messages",
+    {
+      method: "POST",
+
+      headers: {
+        "Content-Type": "application/json"
+      },
+
+      body: JSON.stringify({
+        message: userMessage
+      })
+    }
   );
 
   const data = await response.json();
