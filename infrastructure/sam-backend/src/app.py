08504ec0d9476474d689d5b1c4043960f97952ac
Honeyman-80 <david.w.honeyman@gmail.com>
2026-05-14 15:48:37 -0600

    Update app.py

--- infrastructure/sam-backend/src/app.py
+++ infrastructure/sam-backend/src/app.py
@@ -1,13 +1,52 @@
 import json
+import uuid
+import boto3
+
+dynamodb = boto3.resource("dynamodb")
+
+table = dynamodb.Table("sam-messages-table")
 
 
 def lambda_handler(event, context):
+
+    route_key = (
+        f"{event['requestContext']['http']['method']} "
+        f"{event['rawPath']}"
+    )
+
+    if route_key == "GET /hello":
+        return response(200, {
+            "message": "Hello from SAM Lambda!"
+        })
+
+    if route_key == "POST /message":
+
+        body = json.loads(event.get("body", "{}"))
+
+        message = body.get("message", "")
+
+        item = {
+            "id": str(uuid.uuid4()),
+            "message": message
+        }
+
+        table.put_item(Item=item)
+
+        return response(200, {
+            "message": "Message saved!",
+            "item": item
+        })
+
+    return response(404, {
+        "message": "Route not found"
+    })
+
+
+def response(status_code, body):
     return {
-        "statusCode": 200,
+        "statusCode": status_code,
         "headers": {
             "Content-Type": "application/json"
         },
-        "body": json.dumps({
-            "message": "Hello from SAM Lambda!"
-        })
+        "body": json.dumps(body)
     }
