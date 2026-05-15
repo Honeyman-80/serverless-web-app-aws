107f2c89d33c8b68a62e139f72569b2f7bbcd844
Honeyman-80 <david.w.honeyman@gmail.com>
2026-05-15 10:50:24 -0600

    Create process_message.py

---
+++ infrastructure/sam-backend/src/process_message.py
@@ -0,0 +1,25 @@
+import json
+import boto3
+import os
+
+dynamodb = boto3.resource("dynamodb")
+
+table_name = os.environ["TABLE_NAME"]
+
+table = dynamodb.Table(table_name)
+
+
+def lambda_handler(event, context):
+
+    for record in event.get("Records", []):
+
+        item = json.loads(record["body"])
+
+        table.put_item(Item=item)
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            "message": "Messages processed"
+        })
+    }
